1502949ef6897f54a69b2435a4c29fb500aa9730
yunzhennn1129 <14750694@me.mcu.edu.tw>
2026-09-17 11:49:59 +0800

    week02

---
+++ week02/week02-1.cpp
@@ -0,0 +1,10 @@
+///week02-1.cpp C++ string 字串
+#include <iostream>
+#include <string>
+int main()
+{
+    std::cout << "請問你叫什麼名字啊? " ;
+    std::string name;
+    std::cin >> name;
+    std::cout << name << "你好,今天教字串喔!" ;
+}
---
+++ week02/week02-2.cpp
@@ -0,0 +1,11 @@
+///week02-2.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    cout << "請問你叫什麼名字啊? " ;
+    string name;
+    cin >> name;
+    cout << name << "你好,今天教「命名空間」喔!" ;
+}
